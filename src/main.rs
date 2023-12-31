f3630195b12685363dd4b5424e09ba7e45e5ac09
TheoOliveira <akai.hen@gmail.com>
2023-12-31 02:22:16 -0300

    changes persistent

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use rocket::data::{Data, ToByteUnit};
 use rocket::http::uri::Absolute;
 use rocket::response::status::BadRequest;
 use rocket::tokio::fs::File;
+use rocket::tokio::io::AsyncReadExt;
 use rocket::State;
+use std::path::{Path, PathBuf};
 mod paste_id;
 use paste_id::PasteId;
 use shuttle_persist::PersistInstance;
@@ -15,6 +17,7 @@ const HOST: Absolute<'static> = uri!("http://localhost:8000");
 struct MyState {
     persist: PersistInstance,
 }
+
 #[shuttle_runtime::main]
 async fn main(
     #[shuttle_persist::Persist] persist: PersistInstance,
@@ -47,20 +50,28 @@ async fn retrieve(
 ) -> Result<Option<File>, BadRequest<String>> {
     let str_id = id.to_string();
     let converted_id: &str = &str_id;
-    let file = state
+    let upload_dir: PathBuf = state
         .persist
         .load(converted_id)
         .map_err(|e| BadRequest(e.to_string()))?;
-    //TODO: fix type on the return of the file
-    Ok(File::open(file).await.ok())
+    let file_name = Path::new(&upload_dir);
+    Ok(File::open(&file_name).await.ok())
 }
 
 #[post("/", data = "<paste>")]
 async fn upload(paste: Data<'_>, state: &State<MyState>) -> std::io::Result<String> {
     let id = PasteId::new(ID_LENGTH);
-    paste
+    let str_id = id.to_string();
+    let converted_id: &str = &str_id;
+    // Read the file data into a string
+    let mut buffer = String::new();
+    let _ = paste
         .open(128.kibibytes())
-        .into_file(id.file_path())
-        .await?;
+        .read_to_string(&mut buffer)
+        .await;
+    let _ = state
+        .persist
+        .save(converted_id, &buffer)
+        .map_err(|e| BadRequest(e.to_string()));
     Ok(uri!(HOST, retrieve(id)).to_string())
 }
--- src/paste_id.rs
+++ src/paste_id.rs
@@ -1,9 +1,10 @@
 use rand::{self, Rng};
 use rocket::request::FromParam;
-use std::borrow::Cow;
-use std::path::{Path, PathBuf};
+use rocket::serde::{Deserialize, Serialize};
 
-#[derive(UriDisplayPath)]
+use std::borrow::Cow;
+
+#[derive(UriDisplayPath, Serialize, Deserialize, Clone)]
 pub struct PasteId<'a>(pub Cow<'a, str>);
 
 impl<'a> ToString for PasteId<'a> {
@@ -23,11 +24,6 @@ impl PasteId<'_> {
 
         PasteId(Cow::Owned(id))
     }
-
-    //pub fn file_path(&self) -> PathBuf {
-    //    let root = concat!(env!("CARGO_MANIFEST_DIR"), "/", "upload");
-    //    Path::new(root).join(self.0.as_ref())
-    //}
 }
 
 impl<'a> FromParam<'a> for PasteId<'a> {
